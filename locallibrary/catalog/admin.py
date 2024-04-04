5e68348e4d0e13b5237e8dea72571f91d3f81e13
pressUp2Start <114031379+pressUp2Start@users.noreply.github.com>
2024-04-03 19:59:09 -0500

    Part 4 challenge
    added changes suggested in the challenge yourself section in part 4

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -14,9 +14,13 @@ admin.site.register(Genre)
 #class AuthorAdmin(admin.ModelAdmin):
 #    pass
 
+class BookInLine(admin.TabularInline):
+    model = Book
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInLine]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
@@ -27,6 +31,7 @@ admin.site.register(Author, AuthorAdmin)
 #    pass
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
+    
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -42,6 +47,8 @@ class BookAdmin(admin.ModelAdmin):
 #    pass
 
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'language','due_back', 'id')
+    
     list_filter = ('status', 'due_back')
 
     fieldsets = (
